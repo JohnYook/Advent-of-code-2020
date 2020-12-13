e124f0d69a1257c4f89e865532a45061666610c4
John Yook <45766356+JohnYook@users.noreply.github.com>
2020-12-12 23:38:30 -0500

    day3 part 2

--- src/day3.go
+++ src/day3.go
@@ -38,10 +38,45 @@ func main() {
 		}
 	}
 
-	// go 3 right down 1
+	//solve_1(terrainMap, count, lineLen)
+	solve_2(terrainMap, count, lineLen)
+}
+
+// go 3 right down 1
+func solve_1(terrainMap [][]string, rowCount int, lineLen int) {
+	solution1 := countTrees(terrainMap, rowCount, lineLen, 1, 3)
+	fmt.Printf("Tree count part 1: %d\n", solution1)
+}
+
+func solve_2(terrainMap [][]string, rowCount int, lineLen int) {
+	// Right 1, down 1.
+	// Right 3, down 1.
+	// Right 5, down 1.
+	// Right 7, down 1.
+	// Right 1, down 2.
+	slopes := [5][2]int{{1, 1}, {1, 3}, {1, 5}, {1, 7}, {2, 1}} //{y, x}
+
+	treeCounts := []int{}
+	for k := 0; k < len(slopes); k++ {
+		slope := slopes[k]
+		fmt.Printf("Slope y=%d x=%d\n", slope[0], slope[1])
+		treeCounts = append(treeCounts, countTrees(terrainMap, rowCount, lineLen, slope[0], slope[1]))
+	}
+
+	fmt.Printf("TreeCounts: %v\n", treeCounts)
+
+	total := treeCounts[0]
+
+	for i := 1; i < len(treeCounts); i++ {
+		total *= treeCounts[i]
+	}
+	fmt.Printf("TreeCount multiplied together: %d\n", total) //4355551200
+}
+
+func countTrees(terrainMap [][]string, rowCount int, lineLen int, yIncrement int, xIncrement int) int {
 	treeCount := 0
 	j := 0
-	for i := 0; i < count; i++ {
+	for i := 0; i < rowCount; i += yIncrement {
 		if j >= lineLen {
 			j = j % lineLen
 		}
@@ -49,10 +84,12 @@ func main() {
 			treeCount++
 		} else if terrainMap[i][j] != "." {
 			fmt.Printf("Something is wrong. Neither tree nor open space found: %s\n", terrainMap[i][j])
+			return -1
 		}
 
-		j += 3
+		j += xIncrement
 	}
 
 	fmt.Printf("Tree count: %d\n", treeCount)
+	return treeCount
 }
