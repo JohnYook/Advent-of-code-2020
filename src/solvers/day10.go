43b03786fa77c528ae0c26ff94fc17c6d1a7126d
John Yook <45766356+JohnYook@users.noreply.github.com>
2020-12-20 21:11:54 -0500

    day 10 part 2

--- src/solvers/day10.go
+++ src/solvers/day10.go
@@ -6,6 +6,10 @@ import (
 	"sort"
 )
 
+var _chain []int
+var _chainLen int
+var _numPaths []int
+
 func SolveDay10() {
 	lines, err := helpers.ReadInputFile()
 
@@ -17,20 +21,26 @@ func SolveDay10() {
 
 	adapters := helpers.ConvertToInts(lines)
 
-	getOneJoltTimesThreeJoltDiffs(adapters)
+	_chain = getOutletToDeviceChain(adapters)
+	_chainLen = len(_chain)
+
+	getOneJoltTimesThreeJoltDiffs()
+	fmt.Printf("Number of ways to connect: %d\n", countWaysToConnect())
 }
 
-func getOneJoltTimesThreeJoltDiffs(adapters []int) {
+func getOutletToDeviceChain(adapters []int) []int {
 	chain := make([]int, len(adapters), len(adapters)+2)
 	copy(chain, adapters)
 	chain = append(chain, 0)
 	sort.Ints(chain)
-	chain = append(chain, chain[len(chain)-1]+3)
+	return append(chain, chain[len(chain)-1]+3)
+}
 
+func getOneJoltTimesThreeJoltDiffs() {
 	ones := 0
 	threes := 0
 	last := 0
-	for _, val := range chain {
+	for _, val := range _chain {
 		if val == 0 {
 			continue
 		}
@@ -43,4 +53,31 @@ func getOneJoltTimesThreeJoltDiffs(adapters []int) {
 	}
 
 	fmt.Printf("Jumps of one: %d Jumps of three: %d Product: %d\n", ones, threes, ones*threes)
+}
+
+func countWaysToConnect() int {
+	_numPaths = make([]int, _chainLen)
+	_numPaths[0] = 1
+
+	for i := 1; i < _chainLen; i++ {
+		_numPaths[i] = getPossiblePathsHere(i)
+	}
+
+	return _numPaths[_chainLen-1]
+}
+
+func getPossiblePathsHere(currentIndex int) int {
+	// for i, check i - 1, i - 2, i -3
+	start := currentIndex - 3
+	if start < 0 {
+		start = 0
+	}
+	possiblePaths := 0
+	for i := start; i < currentIndex; i++ {
+		if _chain[currentIndex]-_chain[i] <= 3 {
+			possiblePaths += _numPaths[i]
+		}
+	}
+
+	return possiblePaths
 }
