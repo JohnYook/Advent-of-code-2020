6d5d2485c0a24052d775160d42aa16f5eae61ba5
JohnYook <jyook@gomerits.com>
2020-12-29 03:37:46 -0800

    day 12 part 2

--- src/solvers/day12.go
+++ src/solvers/day12.go
@@ -9,6 +9,8 @@ import (
 var _xPos int
 var _yPos int
 var _heading int
+var _shipX int
+var _shipY int
 
 func SolveDay12() {
 	lines, err := helpers.ReadInputFile()
@@ -26,6 +28,15 @@ func SolveDay12() {
 	moveBoat(lines)
 
 	fmt.Printf("Final position: %d, %d. Manhattan distance from origin: %d\n", _xPos, _yPos, manhattanDistance(_xPos, _yPos))
+
+	// part 2
+	_xPos = 10
+	_yPos = 1
+	_shipX = 0
+	_shipY = 0
+
+	moveBoatPt2(lines)
+	fmt.Printf("Final position Pt2: %d, %d. Manhattan distance from origin: %d\n", _shipX, _shipY, manhattanDistance(_shipX, _shipY))
 }
 
 func moveBoat(lines []string) {
@@ -100,4 +111,91 @@ func moveForward(distance int) {
 
 func manhattanDistance(x int, y int) int {
 	return helpers.Abs(x) + helpers.Abs(y)
+}
+
+func moveBoatPt2(lines []string) {
+	for _, line := range lines {
+		processLinePt2(line)
+	}
+}
+
+func processLinePt2(line string) {
+	action := line[0]
+	value, ok := strconv.Atoi(line[1:])
+	if ok != nil {
+		panic(fmt.Sprintf("Error during processing line: %s\n", line))
+	}
+	switch action {
+	case 'N':
+		_yPos += value
+	case 'S':
+		_yPos -= value
+	case 'E':
+		_xPos += value
+	case 'W':
+		_xPos -= value
+	case 'L':
+		if value < 0 {
+			turnWaypointRight(-value)
+		} else {
+			turnWaypointLeft(value)
+		}
+	case 'R':
+		if value < 0 {
+			turnWaypointLeft(-value)
+		} else {
+			turnWaypointRight(value)
+		}
+	case 'F':
+		moveShipForward(value)
+	default:
+		panic(fmt.Sprintf("Unhandled action in line: %s\n", line))
+	}
+}
+
+func turnWaypointLeft(value int) {
+	degrees := value % 360
+	initX := _xPos
+	initY := _yPos
+	switch degrees {
+	case 0:
+		return
+	case 90:
+		_xPos = -initY
+		_yPos = initX
+	case 180:
+		_xPos = -initX
+		_yPos = -initY
+	case 270:
+		_xPos = initY
+		_yPos = -initX
+	default:
+		panic(fmt.Sprintf("Unexpected boat heading: %d\n", _heading))
+	}
+}
+
+func turnWaypointRight(value int) {
+	degrees := value % 360
+	initX := _xPos
+	initY := _yPos
+	switch degrees {
+	case 0:
+		return
+	case 90:
+		_xPos = initY
+		_yPos = -initX
+	case 180:
+		_xPos = -initX
+		_yPos = -initY
+	case 270:
+		_xPos = -initY
+		_yPos = initX
+	default:
+		panic(fmt.Sprintf("Unexpected boat heading: %d\n", _heading))
+	}
+}
+
+func moveShipForward(units int) {
+	_shipX += _xPos * units
+	_shipY += _yPos * units
 }
